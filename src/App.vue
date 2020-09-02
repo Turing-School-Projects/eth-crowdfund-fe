bda3adb967aea9a32bcd8d691971ef28d6704ac2
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-02 17:45:18 -0500

    Create initial vue boilerplate compiles with parcel

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 class='container'>Hello World</h2>
+</template>
+
+<script lang="js">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: 'app',
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
