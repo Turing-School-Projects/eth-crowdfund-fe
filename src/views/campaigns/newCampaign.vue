3aa0c43655effa9dd67fc3d9561348de10873754
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-03 09:46:05 -0500

    Set up project boilerplate using vue cli

---
+++ src/views/campaigns/newCampaign.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="new-campaign">
+    <div class="new-campaign-form">
+      <h1> A form to create a new campaign would go here </h1>
+    </div>
+    <router-link to='/campaigns'>Back Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewCampaign"
+};
+</script>
---
+++ src/views/campaigns/singleCampaign.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="single-campaign">
+    <div class="single-campaign-info">
+      <h1>This route would display a single Campaigns details and the option for a
+        user to contribute to said campaign</h1>
+    </div>
+  <router-link to='/campaigns'>Back Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleCampaign',
+};
+</script>
