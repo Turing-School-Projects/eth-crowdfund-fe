84f8bbe1dbf400d752781d6fa4d958ffac9ebed4
Jack Cullen <jpcullen20@gmail.com>
2020-09-04 17:19:51 -0600

    Add smart contracts

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,18 +1,5 @@
 pragma solidity >=0.4.22 <0.7.0;
 
-contract CampaignFactory {
-    address[] public deployedCampaigns;
-
-    function createCampaign(uint minimum) public {
-        Campaign newCampaign = new Campaign(minimum, msg.sender);
-        deployedCampaigns.push(address(newCampaign));
-    }
-
-    function getDeployedCampaigns() public view returns(address[] memory) {
-        return deployedCampaigns;
-    }
-}
-
 contract Campaign {
     struct Request {
         string description;
