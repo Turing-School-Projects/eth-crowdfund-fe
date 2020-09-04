d9fc6cab1d7935d7d004e156bdd1737306c6afbb
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-04 14:15:54 -0500

    Connect all campaigns page to store

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,28 @@ export default createStore({
   state: {
     campaigns: [
       {
+        id: 1,
         title: "Buy a well",
         description: "Clean water for rural community",
         minContribution: 100
       },
       {
-        title: "Clean up community park",
-        description: "Help provide tools and snacks for Park Clean Up day",
+        title: "End World Hunger",
+        description: "We can do it",
+        id: 2,
+        completed: false
+      },
+      {
+        title: "Clean up Cheesman park",
+        description: "Its right by my house",
+        id: 3,
         minContribution: 35
+      },
+      {
+        title: "Help orphaned children",
+        description: "They really need it",
+        id: 4,
+        completed: false
       }
     ]
   },
