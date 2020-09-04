049b1bcd3762e5c5c03157610fc62404307b23ed
Andy Tom <andrew.tom@colorado.edu>
2020-09-03 18:41:59 -0600

    sets up campaign container and campaign cards

---
+++ src/components/CampaignCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section class="campaign-card">
+      <h1> {{campaign.title}}</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CampaignCard',
+  props: ["campaign"]
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.campaign-card {
+  border: 1px solid black;
+  background: #42b983;
+}
+
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section class="campaign-container">
+      <h1> Campaign Container</h1>
+      <div v-for="campaign in campaigns" v-bind:key="campaign.id">
+        <CampaignCard v-bind:campaign="campaign" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+import CampaignCard from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Container',
+  props: ["campaigns"],
+  components: {
+    CampaignCard
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
