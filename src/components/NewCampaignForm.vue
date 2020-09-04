06f3d1833e463584b855f5d9482477d3e59c6d58
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-03 19:10:11 -0500

    Create new campaign form component

---
+++ src/components/NewCampaignForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form>
+    <input v-model='title' placeholder='Campaign Title' />
+    <input v-model='minContribution' placeholder='Minimun Wei Contribution' />
+    <textarea v-model='description' placeholder='Describe your Campaign...'></textarea>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'NewCampaignForm',
+  data() {
+    return {
+      title: '',
+      description: '',
+      minContribution: 0
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
--- src/views/campaigns/newCampaign.vue
+++ src/views/campaigns/newCampaign.vue
@@ -2,13 +2,19 @@
   <div class="new-campaign">
     <div class="new-campaign-form">
       <h1> A form to create a new campaign would go here </h1>
+      <new-campaign-form></new-campaign-form>
     </div>
     <router-link to='/'>Back Home</router-link>
   </div>
 </template>
 
 <script>
+import NewCampaignForm from '@/components/NewCampaignForm.vue'
+
 export default {
-  name: 'NewCampaign'
+  name: 'NewCampaign',
+  components: {
+    NewCampaignForm
+  }
 }
 </script>
