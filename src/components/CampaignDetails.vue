3c3b02078bb934c639dee39284668c9355108846
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-05 10:36:04 -0500

    Connect campaign details component to route params

---
+++ src/components/CampaignDetails.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h2>{{ $route.params.address }}</h2>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'CampaignDetails',
+  props: ['address']
+}
+</script>
+
+<style scoped lang='scss'>
+</style>
