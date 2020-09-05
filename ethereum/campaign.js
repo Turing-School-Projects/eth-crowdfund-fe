64632740aa4c9feb2a954faf7835aa0fe0529c64
Jack Cullen <jpcullen20@gmail.com>
2020-09-05 13:30:44 -0600

    Add ability to get a campaign instance and remove compile and deploy scripts

--- ethereum/campaign.js
+++ ethereum/campaign.js
@@ -1,6 +1,9 @@
 import web3 from "./web3";
 import Campaign from "./build/Campaign.json";
 
-export default (address) => {
-  return new web3.eth.Contract(JSON.parse(Campaign.interface), address);
-};
+const contract = require("@truffle/contract");
+
+const CampaignContract = contract(Campaign)
+CampaignContract.setProvider(web3.currentProvider);
+
+export default CampaignContract
