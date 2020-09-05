06f58a982abcf1b4ec45b824c54355d8e7fa0eeb
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-05 11:11:48 -0500

    Display campaign information to dom

--- src/components/CampaignDetails.vue
+++ src/components/CampaignDetails.vue
@@ -1,6 +1,12 @@
 <template>
   <section>
-    <h2>{{ $route.params.address }}</h2>
+    <h3> Campaing Title: </h3>
+    <h2>{{ campaign.title }}</h2>
+    <h3> Description: </h3>
+    <h4>{{ campaign.description }}</h4>
+    <h3> Minimum Contribution </h3>
+    <h1>{{ campaign.minContribution }}</h1>
+    <p>There is some information missing and this has no styles </p>
   </section>
 </template>
 
