d302e3fb3131f4f3dbe15b375e5ca7a595bca821
Jack Cullen <jpcullen20@gmail.com>
2020-09-05 10:39:16 -0600

    Setup connection to factory

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,11 @@
 <script>
 // @ is an alias to /src
 import CampaignContainer from '@/components/Container.vue';
+import factory from '../../ethereum/factory'
+
+factory.at('0x7c70286f6991c660a0cC6d52A74aEBbDE45Da380')
+  .then((instace) => instace.getDeployedCampaigns())
+  .then((campaignAddresses) => console.log(campaignAddresses))
 
 export default {
   name: 'Home',
