6287db3af28d4a0b93adb23b1499902db1f776c5
Andy Tom <andrew.tom@colorado.edu>
2020-09-05 20:13:54 -0600

    adds styling for grid to be more centered and 4 campaigns on the page

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1> Campaign Container</h1>
+    <div class="about">
+    </div>
+    <h2>Top Fundraisers</h2>
     <section class="campaign-container">
       <div v-for="campaign in campaigns" v-bind:key="campaign.id">
         <CampaignCard v-bind:campaign="campaign" />
@@ -25,9 +27,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
   .campaign-container {
+    margin: 1rem 0rem 4rem 4rem;
     display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 5vh;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: .5rem;
   }
+
 </style>
