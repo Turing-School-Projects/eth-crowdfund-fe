47266009db0dde9dd973e09e086d8b76932d8c94
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-07 18:47:50 -0500

    Change module import syntax to ES5

--- src/contracts/factory.js
+++ src/contracts/factory.js
@@ -1,9 +1,9 @@
 import web3 from "./web3";
-import CampaignFactory from "./build/CampaignFactory.json";
 
 const contract = require("@truffle/contract");
+const CampaignFactory = require("./build/CampaignFactory.json");
 
-const Factory = contract(CampaignFactory)
+const Factory = contract(CampaignFactory);
 Factory.setProvider(web3.currentProvider);
 
-export default Factory
+export default Factory;
