eae2c555070af3a371784719893f075f117b40d1
Andy Tom <andrew.tom@colorado.edu>
2020-09-08 17:21:38 -0600

    adds basic styling to campaign details

--- src/components/CampaignDetails.vue
+++ src/components/CampaignDetails.vue
@@ -1,20 +1,23 @@
 /* eslint-disable max-len */
 <template>
-  <section>
-    <h3> Campaing Title: </h3>
-    <h2>{{ campaign.name }}</h2>
-    <h3> Description: </h3>
-    <h4>{{ campaign.description }}</h4>
-    <h3> Minimum Contribution </h3>
-    <img :src=campaign.image />
-    <h1>{{ campaign.min_contribution }}</h1>
-    <p>There is some information missing and this has no styles </p>
-    <input v-model='contribution' placeholder='Enter your contribution' type="number"/>
-    <button :disabled="!contribution"
-    @click="submitContribution">
-    Submit Contribution </button>
-
-  </section>
+  <div class="details-card">
+    <section>
+      <div>
+        <img :src=campaign.image />
+      </div>
+      <div class= "campaign-text">
+      <h2>{{ campaign.name }}</h2>
+      <h4>{{ campaign.description }}</h4>
+      <p>Minimum Contribution  <b> {{ campaign.min_contribution }}</b></p>
+      <div>Make a contribution</div>
+      <input v-model='contribution' placeholder='Enter your contribution' type="number"/>
+      <button :disabled="!contribution"
+      @click="submitContribution">
+      Submit Contribution </button>
+      <div v-if="userMessage"> Please enter a contribution over {{campaign.min_contribution}}</div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -40,17 +43,48 @@ export default {
   methods: {
     async submitContribution() {
       // const campaignAddress = "0xe8a0980C2B37C2C4FCE45e579301B00CC824bCFc"
+      if (this.contribution <= this.campaign.min_contribution) {
+        this.userMessage = true;
+        return
+      }
       const campaignInstance = await ethCampaign.at(this.address);
       campaignInstance.contribute({ from: this.$store.state.accountNum, value: this.contribution })
     }
   },
   data() {
     return {
-      contribution: 0
+      contribution: 0,
+      userMessage: null
     }
   }
 }
 </script>
 
 <style scoped lang='scss'>
+section {
+  display: flex;
+  justify-content: center;
+  text-align: left;
+}
+
+img {
+  width: 20rem;
+  height: 30rem;
+}
+
+.details-card {
+  width: 40rem;
+  margin: auto;
+  margin-top: 3em;
+}
+
+input {
+  display: block;
+  margin: 0rem 1rem 1rem 0rem;
+}
+
+.campaign-text {
+  margin-left: 2rem;
+}
+
 </style>
