e86312492605d2e71cf93f2091450202caf130a5
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-08 15:51:05 -0500

    Begin creating new campaign action

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import factory from "../contracts/factory";
 import campaign from "../contracts/campaign";
 import web3 from "../contracts/web3";
+import API_URL from "../env";
 
 export default createStore({
   state: {
@@ -86,13 +87,19 @@ export default createStore({
       commit("setFactory", instance);
     },
     fetchCampaign: async ({ commit }, address) => {
-      console.log('campaignAddress', address)
+      console.log("campaignAddress", address);
       const instance = await campaign.at(address);
       commit("setCampaign", instance);
     },
     fetchAccountNum: async ({ commit }) => {
       const accounts = await web3.eth.getAccounts();
       commit("setAccountNum", accounts[0]);
+    },
+    createBECampaign: async ({ commit }, campaignRequest) => {
+      const accounts = await web3.eth.getAccounts();
+      const newCampaign = { ...campaignRequest, manager: accounts[1] };
+      const response = await axios.post(`${API_URL}campaigns`, newCampaign);
+      commit("ADD_CAMPAIGN", response);
     }
   },
   modules: {}
