ad76c65829e322568a5f698809f8950a7388d808
Andy Tom <andrew.tom@colorado.edu>
2020-09-10 10:59:11 -0600

    adds adds styling to your campaigns view

--- src/views/campaigns/userCampaigns.vue
+++ src/views/campaigns/userCampaigns.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="user-campaigns" v-for="campaign in userCampaigns" v-bind:key="campaign.id">
-    <section class="campaign-card">
-      <section>
-      <div class="img-div"><img :src=campaign.image /></div>
-      <div class="campaign-info">
-        <h1> {{campaign.name}}</h1>
-        <p>{{campaign.description}}</p>
-      </div>
+  <div class="container">
+    <div class="user-campaigns" v-for="campaign in userCampaigns" v-bind:key="campaign.id">
+      <section class="campaign-card">
+        <section>
+        <div class="img-div"><img :src=campaign.image /></div>
+        <div class="campaign-info">
+          <h1> {{campaign.name}}</h1>
+          <p>{{campaign.description}}</p>
+        </div>
+        </section>
+        <div class="requests"><b>Total Request:</b> {{campaign.requests.length}}</div>
+        <router-link
+          style="text-decoration: none;"
+          :to="{ name: 'Campaign Request',
+          params: {id: campaign.id, address: campaign.address}}">
+          Create A Request
+        </router-link>
       </section>
-      <div class="requests"><b>Total Request:</b> {{campaign.requests.length}}</div>
-    </section>
-    <router-link
-      style="text-decoration: none;"
-      :to="{ name: 'Campaign Request',
-      params: {id: campaign.id, address: campaign.address}}">
-      Create A Request
-    </router-link>
+    </div>
   </div>
 </template>
 
@@ -42,7 +44,43 @@ export default {
 }
 
 img {
-  height: 60vh;
-  width: 45vw;
+  width: 100%;
+  height: 100%;
+  align-self: flex-end;
 }
+
+.container {
+  margin: 1rem 0rem 4rem 4rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr ;
+  grid-gap: .5rem;
+}
+
+.campaign-card {
+  background: whitesmoke;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  text-decoration: none;
+  box-shadow: 1px 1px 3px grey;
+  height: 60vh;
+  width: 40vw;
+  box-sizing: border-box;
+  transform: scale(1);
+  color: black;
+  &:hover {
+    transform: scale(1.02);
+    box-shadow: 2px 2px 10px black;
+    transition: all 0.25s;
+    outline: 1px solid black;
+  }
+}
+
+.img-div {
+  height: 30vh;
+  width: 20vw;
+  background: aqua;
+}
+
 </style>
