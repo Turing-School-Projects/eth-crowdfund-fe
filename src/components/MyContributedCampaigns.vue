20a3fb6fa72af784aef17707168e87ae367a191b
Andy Tom <andrew.tom@colorado.edu>
2020-09-10 15:14:40 -0600

    adds button with hard coded account info for approval

--- src/components/MyContributedCampaigns.vue
+++ src/components/MyContributedCampaigns.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="hello">
     <h1>Contributed Campaigns</h1>
+    <h3>Campaign: Mullets</h3>
+    <p>Description:More haircuts</p>
+    <p>Amount: 1 Eth</p>
+    <button @click="approveRequest"> Approve This Request</button>
+    <p>Description: Less haircuts</p>
+    <p>Amount: 1 Eth</p>
+    <button> Approve This Request</button>
   </div>
 </template>
 
 <script>
 
+import Campaign from "../contracts/campaign";
+import web3 from "../contracts/web3";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    /* eslint-disable */
+    async approveRequest() {
+      const campaignInstance = await Campaign.at("0x933B53269E494Ec2D2A3349B84CF28561c56679c")
+      const accounts = await web3.eth.getAccounts();
+      console.log(campaignInstance)
+      const result = await campaignInstance.approveRequest(0, { from: this.$store.state.accountNum, gas: "1000000"})
+      const request = await campaignInstance.request(0)
+      console.log('result', result)
+      console.log('request', request)
+    }
+  },
+  created() {
+    // Get all campaigns I have voting power
+    // From Antonio
   }
 }
 </script>
