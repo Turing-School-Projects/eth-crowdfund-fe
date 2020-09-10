732892b4b325fcda7d18f419959294d6e3c37b97
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-10 14:35:13 -0500

    Add contributors

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,38 @@
     <h1>This is under construction.</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    contributors: {
+      BE: [
+        {
+          name: 'Antonio Jackson',
+          gitHub: 'https://github.com/AntonioJacksonII'
+        },
+        {
+          name: 'Ross Perry',
+          gitHub: 'https://github.com/perryr16'
+        },
+        {
+          name: 'Taylor Keller',
+          gitHub: 'https://github.com/takeller'
+        }
+      ],
+      FE: [
+        {
+          name: 'Andrew Tom',
+          gitHub: 'https://github.com/attom2'
+        },
+        {
+          name: 'Edwin Montealvo',
+          gitHub: 'https://github.com/edmdc89'
+        }
+      ],
+      BC: [{name: 'Jack Cullen', gitHub: 'https://github.com/jpc20'}]
+    }
+  }
+}
+</script>
