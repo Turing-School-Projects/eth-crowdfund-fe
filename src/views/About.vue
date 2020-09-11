73714013fd6425840a8bd1e07d76d9e915fac79c
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-10 21:48:42 -0500

    Add brief project description

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is under construction.</h1>
-    <SideBar class='side-bar' />
+    <SideBar class='sidebar'/>
+    <section class='about-content'>
+      <h2>Turing School of Software & Design Capstone Project</h2>
+      <p>The final project at Turing school is a full-stack venture to create an app
+        around an idea pitched by one of the students. Our goal was to develop a
+        crowdfunding dApp that made use of blockchain technology.
+      </p><p>
+        We had two weeks to
+        wireframe a front-end to interact with the blockchain and a back-end to supplement
+        it. We decided to use <b>Python</b> (+ ...other technologies) for our database and
+        <b>Vue</b> (+ ...other technologies) for our front-end. To interact with the blockchain,
+        the truffle framework/environment was used.<b>*None of these technologies were
+        explicitly taught at Turing*</b>
+      </p>
+      <h3>Why use blockchain?</h3>
+      <h3>Working wiht the blockchain</h3>
+      <h3>Back-End Technologies</h3>
+    </section>
   </div>
 </template>
 
@@ -19,7 +35,20 @@ export default {
 <style lang='scss'>
 @import "../_variables.scss";
 
-.side-bar {
-  background-color: $light-green;
+.about {
+  display: grid;
+  margin-top: 3rem;
+  grid-template-columns: 1fr 2fr;
+
+  .sidebar {
+    margin-right: 0.3rem;
+    width: 13rem;
+  }
+
+  .about-content {
+    @include beautifyBorder(.8rem, $light-green, $green, $bg_2, $dark-blue, $bg_1, $blue)
+    margin-left: 0rem;
+    margin-right: 1.3rem;
+  }
 }
 </style>
