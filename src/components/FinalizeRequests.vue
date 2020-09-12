20faa707d6797019ea10cdc84003887a517d81ed
Andy Tom <andrew.tom@colorado.edu>
2020-09-11 22:26:31 -0500

    adds loading screen to finalize and approve

--- src/components/FinalizeRequests.vue
+++ src/components/FinalizeRequests.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div v-if="!loading">
     <h1>Managed Requests</h1>
     <h3>Campaign: Mullets</h3>
     <p>Description:More haircuts</p>
@@ -9,20 +9,31 @@
     <p>Amount: 1 Eth</p>
     <button> Finalize This Request</button>
   </div>
+  <Loading v-if="loading" />
 </template>
 
 <script>
 import Campaign from "../contracts/campaign";
+import Loading from "./Loading.vue";
 
 export default {
-  name: 'HelloWorld',
+  name: 'FinalizeRequest',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      loading: false
+    }
+  },
+  components: {
+    Loading
   },
   methods: {
     /* eslint-disable */
     async finalizeRequest() {
-      const campaignInstance = await Campaign.at("0xcd579f2539a1Ed7753Dd74D065656680C93d7737")
+      this.loading = true
+      const campaignInstance = await Campaign.at("0x8378944D41237C286592a2f7510BD48C1c3dc8F0")
       const result = await campaignInstance.finalizeRequest(1, { from: this.$store.state.accountNum, gas: "1000000"})
       const request = await campaignInstance.requests(1)
       console.log('request', request)
