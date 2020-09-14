9c30793f29e1f0ab9491f35a93bc692fd162b0c0
Jack Cullen <jpcullen20@gmail.com>
2020-09-14 08:36:46 -0600

    Add first draft of why page

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
       <div @click="view = 'how-to'" v-bind:class="{ active: view === 'how-to' }">How
         to?</div>
       <div @click="view = 'why'" v-bind:class="{ active: view === 'why' }">Why
-        Blockchain?</div>
+        use EthoBoost?</div>
       <div @click="view = 'technologies'" v-bind:class="{ active: view === 'technologies'
         }">Technologies</div>
       <div @click="view = 'contributors'" v-bind:class="{ active: view ===
@@ -15,7 +15,53 @@
       <HowTo />
     </section>
     <section v-if="view === 'why'">
-      <h2>Why use Blockchain?</h2>
+      <h2>Why use Ethereum?</h2>
+      <p>EthoBoost identifies several major problems with the crowdfunding industry
+        and aims to fix them.
+      </p>
+      <p>
+        <b>Trust:</b> Potential donors are less likely to contribute to funds run
+        by unknown brands because once the funds are recieved, they have no say in
+        how the money is spent and have no reason to trust that the fund manager will spend the funds
+        . EthoBoost give donors the power to review and approve
+        requests to withdraw the funds before the manager can complete each withdrawal.
+        Fund managers set the minimum value that a donor must contribute in order
+        to have approval rights for that fund. When a contributor donates a value
+        higher than that threshold they become an approver of that campaign. Any time
+        a fund manager wants to withdraw money they do so by creating a request to
+        withdraw. In order for that request to go through and the funds to be distributed
+        the request has to be approved by over 50% of approver for the campaign.
+      </p>
+      <p>
+        <b>Borderless:</b>Our current financial system makes it extremely difficult
+        to send and receive money across borders resulting in high fees to wire money
+        internationally. Many crowdfunding platforms are restricted to their home country
+        and others charge an extra fee for international transactions. EthoBoost is
+        built on the Ethereum blockchain which means you don't need a bank accunt
+        from any specific country to transact on EthoBoost.
+      </p>
+      <p>
+        <b>Transactions(Fees and time):</b>  Crowdfunding platforms generally charge
+        a transaction fee of around 5%. These transactions take at least 3-5 business
+        days to complete. Transactions on EthoBoost, facilitated by the Ethereum blockchain,
+        cost less than 1 cent and complete in 30 seconds or less.
+      </p>
+      <p>
+        <b>Decentralized:</b> The Ethereum blockchain powers our payment system
+        which means all of the financial transactions happen peer to peer. We don't
+        need your bank account information and we don't have direct access to any of
+        the funds stored in the campaigns on our platform. If anything were to happen
+        to our application, the funds would continue to exist on the blockchain.
+      </p>
+      <p>
+        <b>Transparency???:</b>
+      </p>
+      <p>
+        <b>Anonymous???:</b>
+      </p>
+      <p>
+        <b>Permanent???:</b>
+      </p>
     </section>
     <section v-if="view === 'technologies'" class='about-content'>
       <h2>Turing School of Software & Design Capstone Project</h2>
