2264a49bd216e92781a9b0fa3665fddd70db0754
Jack Cullen <jpcullen20@gmail.com>
2020-09-15 15:19:32 -0600

    Change order in why section

--- src/components/About/why.vue
+++ src/components/About/why.vue
@@ -3,18 +3,18 @@
     <h2>Why use EthoBoost?</h2>
     <h3><i><b>Etho Boost</b> is changing crowdfunding</i></h3>
     <p>
+      <b><i>Transparency</i></b>:
+      Donors have the power to approve withdrawal requests ensuring that donations
+      are used as advertised.
+      <br><br>
+      <b><i>The Approval Process</i></b>: Booster managers set a minimum value that a
+      donor must contribute in order to have approval rights for that booster.
+      51% of approvers are required to remove funds from a booster.
+      <br><br>
       <b><i>Efficiency</i></b>:
         Complete transactions in 30 seconds or less! Most crowdfunding apps take 3-5
         business days to complete. Taking advantage of the Etherium Blockchain and
         digital wallets makes this possible.
-      <br><br>
-      <b><i>Transparency</i></b>:
-        Donors have the power to approve withdrawal requests ensuring that donations
-        are used as advertised.
-      <br><br>
-      <b><i>The Approval Process</i></b>: Booster managers set a minimum value that a
-        donor must contribute in order to have approval rights for that booster.
-        51% of approvers are required to remove funds from a booster.
       <br><br>
       <b><i>Borderless + Bankless</i></b>: Built on the Ethereum blockchain, Etho Boost
         grants donors and fundraisers the freedom to exchange funds without the
