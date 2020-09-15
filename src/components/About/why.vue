4b2e54487d222688c440baa1c4f5ac234fa45d83
Ross Perry <perryr16@gmail.com>
2020-09-15 12:56:16 -0600

    remove '&'

--- src/components/About/why.vue
+++ src/components/About/why.vue
@@ -9,7 +9,7 @@
       <br><br>
       <b><i>The Approval Process</i></b>: Booster managers set a minimum value that a donor must contribute in order to have approval rights for that booster. 51% of approvers are required to remove funds from a booster.
       <br><br>
-      <b><i>Borderless & Bankless</i></b>: Built on the Ethereum blockchain, Etho Boost grants donors and fundraisers the freedom to exchange funds without the constraints of a bank account.
+      <b><i>Borderless + Bankless</i></b>: Built on the Ethereum blockchain, Etho Boost grants donors and fundraisers the freedom to exchange funds without the constraints of a bank account.
       <br><br>
       <b><i>Lower Fees</i></b>: Transactions on Etho Boost cost less than 1 cent! while most crowdfunding fees are 5% and are incurred by middlemen and international transaction costs.
       <br><br>
