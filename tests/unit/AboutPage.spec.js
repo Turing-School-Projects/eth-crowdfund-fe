1e8a8207c118097506e3bb8a2cd1b2c39073951b
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-15 15:01:17 -0500

    Complete unit testing About page

--- tests/unit/AboutPage.spec.js
+++ tests/unit/AboutPage.spec.js
@@ -3,8 +3,9 @@ import About from "@/views/About.vue";
 import HowTo from "@/components/About/howTo.vue";
 import Why from "@/components/About/why.vue";
 import Technologies from "@/components/About/Technologies.vue";
+import Contributors from "@/components/About/Contributors.vue";
 
-describe("truth", () => {
+describe("about page content", () => {
   it("should display four info tabs", () => {
     const wrapper = shallowMount(About);
     const tabs = wrapper.findAll(".tab");
@@ -24,9 +25,7 @@ describe("truth", () => {
     expect(header.text()).toEqual("How to use Etho-Boost");
     expect(subHeader.length).toEqual(4);
   });
-});
 
-describe("about page wiht testing library", () => {
   it("should also show info on the blockchain", () => {
     const wrapper = mount(About, {
       data() {
@@ -54,6 +53,25 @@ describe("about page wiht testing library", () => {
     const tabview = wrapper.findComponent(Technologies);
     const technologiesHeader = tabview.findAll("h3");
 
-    expect;
+    expect(technologiesHeader[0].text()).toEqual("Back-End Technologies");
+    expect(technologiesHeader[1].text()).toEqual("Front-End Technologies");
+  });
+
+  it("should show contributors names as links", () => {
+    const wrapper = mount(About, {
+      data() {
+        return {
+          view: "contributors"
+        };
+      }
+    });
+
+    const contributors = wrapper.findComponent(Contributors);
+    const teams = contributors.findAll("h3");
+    const teamNames = teams.map((team) => team.text());
+    const members = contributors.findAll("a");
+
+    expect(teamNames).toEqual(["Front End Team", "Back End Team"]);
+    expect(members).toHaveLength(6);
   });
 });
