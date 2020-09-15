4088ca29716e718e6e6a8f4c14ed1205c62e97ff
Jack Cullen <jpcullen20@gmail.com>
2020-09-14 21:31:36 -0600

    Refactor how to and why pages

---
+++ src/components/About/why.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class='why-content'>
+    <h2>Why use EthoBoost?</h2>
+    <p>The crowdfunding industry is generally inefficient and convoluted.
+      EthoBoost is a transparent alternative that offers autonomy and accountability
+      to donors and fundraisers.
+    </p>
+    <p>
+      <b>Trust:</b> The untrustworthy nature of crowdfunding websites deters potential
+      donors because there is no accountability. Fundraisers are not expected to publish
+      honest, detailed information about where a donated dollar goes, and EthoBoost
+      demands more transparency. EthoBoost gives donors the power to review and
+      approve requests to withdraw funds before a manager can complete a withdrawal,
+      ensuring that donations are used as advertised. Fund managers set a minimum value
+      that a donor must contribute in order to have approval rights for that fund. A
+      fund manager must create a request to withdraw donations, and the request must be
+      approved by over 50% of ‘approvers’ for the campaign.
+    </p>
+    <p>
+      <b>Borderless:</b> Today’s financial system makes it extremely difficult to
+      send and receive money across borders, forcing crowdfunding platforms to restrict
+      transactions by imposing fees on international donations. EthoBoost is built on
+      the Ethereum blockchain, granting donors and fundraisers the freedom to exchange
+      funds without the constraints of a bank account.
+    </p>
+    <p>
+      <b>Transactions(Fees and time):</b> Crowdfunding platforms generally charge
+      a transaction fee of around 5%. These transactions take at least 3-5 business
+      days to complete. Transactions on EthoBoost, facilitated by the Ethereum blockchain,
+      cost less than 1 cent and complete in 30 seconds or less.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Why'
+}
+</script>
+
+<style>
+.why-content {
+  background: #ccc;
+}
+</style>
