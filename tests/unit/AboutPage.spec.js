84221d0d6803da3594dd5ed3967cda64273ee377
EdMo <60306770+emontealvo@users.noreply.github.com>
2020-09-14 21:09:46 -0500

    Add test for how to page

--- tests/unit/AboutPage.spec.js
+++ tests/unit/AboutPage.spec.js
@@ -1,18 +1,35 @@
-import { shallowMount, createLocalVue } from "@vue/test-utils";
-import VueRouter from "vue-router";
+import { shallowMount, mount } from "@vue/test-utils";
 import About from "@/views/About.vue";
-
-// const localVue = createLocalVue();
-// localVue.use(VueRouter);
-// const router = new VueRouter();
+import HowTo from "@/components/About/howTo.vue";
 
 describe("truth", () => {
-  it("should hold true", () => {
+  it("should display four info tabs", () => {
     const wrapper = shallowMount(About);
     const tabs = wrapper.findAll(".tab");
     expect(tabs[0].text()).toEqual("How to?");
     expect(tabs[1].text()).toEqual("Why Blockchain?");
     expect(tabs[2].text()).toEqual("Technologies");
     expect(tabs[3].text()).toEqual("Contributors");
+  });
+
+  it("should default to display a how to page", () => {
+    const wrapper = mount(About);
+
+    const content = wrapper.findComponent(HowTo);
+    const header = content.find("h2");
+    const subHeader = content.findAll("h3");
+
+    expect(header.text()).toEqual("How to use Etho-Boost");
+    expect(subHeader.length).toEqual(4);
+    wrapper.destroy();
+  });
+
+  it("should also show info on the blockchain", () => {
+    const wrapper = mount(About);
+    wrapper.findAll(".tab")[1].trigger("click");
+
+    const header = wrapper.find("h2");
+
+    expect(header.text()).toEqual("Why use Blockchain?");
   });
 });
